0d8d9374fd02f3d3c577477e7088136a6de85dca
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-06 04:57:04 +0700

    week 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Color;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private int donate = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Custom tool bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Donation.1.5");
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.inflateMenu(R.menu.toolbar_menu);
+
+
     }
 }
