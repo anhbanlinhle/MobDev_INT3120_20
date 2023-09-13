c23ae383989b51994b7ad891cf761cfe93bc4601
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-13 22:21:42 +0700

    constraint layout for order

--- Lesson_4_2/app/src/main/java/com/example/lesson_4_2/MainActivity.java
+++ Lesson_4_2/app/src/main/java/com/example/lesson_4_2/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.order_base);
-        setContentView(R.layout.order_linear);
+//        setContentView(R.layout.order_linear);
+//        setContentView(R.layout.order_relative);
+        setContentView(R.layout.order_constraint);
+//        setContentView(R.layout.order_table);
     }
 }
