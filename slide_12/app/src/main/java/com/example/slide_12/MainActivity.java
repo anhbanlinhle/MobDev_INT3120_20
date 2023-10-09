c6354c3b9ce78e7a98b44f6fdf0348941b84585f
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-09 22:11:30 +0700

    base layout for main

--- slide_12/app/src/main/java/com/example/slide_12/MainActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.slide_12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void accelerometer(View view) {
+    }
+
+    public void magnetometer(View view) {
+
+    }
+
+    public void wifi(View view) {
+
+    }
+
+    public void call(View view) {
+
+    }
+
+    public void sms(View view) {
+
+    }
+
+    public void camera(View view) {
+
+    }
+
+    public void bluetooth(View view) {
+
+    }
+
 }
