5adacbe70c2cf265c2589684f3db8af629300ad1
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-10 03:17:20 +0700

    add acceleromter sensor with button

--- slide_12/app/src/main/java/com/example/slide_12/AccelerometerActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/AccelerometerActivity.java
@@ -11,12 +11,14 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
     ConstraintLayout screen;
     SensorManager manager;
     Sensor accelerometer;
     Button enableAccelerometer;
+    TextView xText, yText, zText;
     int prevColor = Color.WHITE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +26,11 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         setContentView(R.layout.activity_accelerometer);
 
         screen = findViewById(R.id.screen);
-
         enableAccelerometer = findViewById(R.id.enableAccelerometer);
         enableAccelerometer.setEnabled(false);
+        xText = findViewById(R.id.x);
+        yText = findViewById(R.id.y);
+        zText = findViewById(R.id.z);
 
         manager = (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -38,16 +42,12 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
     public void triggerAccelerometer(View view) {
         if (enableAccelerometer.getText().equals("DISABLED") ) {
             enableAccelerometer.setText("ENABLED");
+            manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
         else {
             enableAccelerometer.setText("DISABLED");
+            manager.unregisterListener(this);
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -57,6 +57,29 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
     }
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        xText.setText("x-a: " + x);
+        yText.setText("y-a: " + y);
+        zText.setText("z-a: " + z);
+
+        int color = prevColor;
+
+        if (x > 5 || y > 5 || z > 5) {
+            color = Color.WHITE;
+        } else if (x < -5 || y < -5 || z < -5) {
+            color = Color.BLACK;
+        }
+
+        if (color != prevColor) {
+            xText.setTextColor(prevColor);
+            yText.setTextColor(prevColor);
+            zText.setTextColor(prevColor);
+            prevColor = color;
+            screen.setBackgroundColor(color);
+        }
 
     }
 
