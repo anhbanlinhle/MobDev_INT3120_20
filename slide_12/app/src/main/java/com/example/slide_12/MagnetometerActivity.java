9d949ff51652588bb9785e746052df72f4f72cbf
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-10 17:37:53 +0700

    base layout for magnetometer sensor

--- slide_12/app/src/main/java/com/example/slide_12/MagnetometerActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/MagnetometerActivity.java
@@ -2,9 +2,17 @@ package com.example.slide_12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MagnetometerActivity extends AppCompatActivity {
+    TextView degree;
+    ImageView compass;
+    SensorManager manager;
+    Sensor magnetometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
