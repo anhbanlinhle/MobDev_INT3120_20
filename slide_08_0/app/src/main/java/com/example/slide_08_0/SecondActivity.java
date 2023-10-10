d68583df7afbc978c4cc2de31568230346346b80
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-10 23:53:09 +0700

    slide_8 -> slide_08_0

---
+++ slide_08_0/app/src/main/java/com/example/slide_08_0/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.slide_08_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView id;
+    TextView name;
+    TextView status;
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side);
+
+        id = findViewById(R.id.userId);
+        name = findViewById(R.id.userName);
+        status = findViewById(R.id.status);
+        back = findViewById(R.id.back);
+
+        Intent intent = getIntent();
+        id.setText(intent.getStringExtra("id"));
+        name.setText(intent.getStringExtra("name"));
+        status.setText(intent.getBooleanExtra("status", true) ? "Online" : "Offline");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
