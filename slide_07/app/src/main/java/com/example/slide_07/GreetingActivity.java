0bdfc3d0b1d4d6a96f08c8f0a7ee89b587f8bfcc
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-10 23:31:19 +0700

    [refactor] base layout for second activity

---
+++ slide_07/app/src/main/java/com/example/slide_07/GreetingActivity.java
@@ -0,0 +1,48 @@
+package com.example.slide_07;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GreetingActivity extends AppCompatActivity {
+    Button btnBack;
+    String fullName;
+    String message;
+    TextView textViewMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greeting);
+        btnBack = findViewById(R.id.btnBack);
+        textViewMessage = findViewById(R.id.textViewMessage);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goBack();
+            }
+        });
+        Intent intent = getIntent();
+        fullName = intent.getStringExtra("fullName");
+        message = intent.getStringExtra("message");
+        textViewMessage.setText(message);
+    }
+
+    public void goBack() {
+        onBackPressed();
+    }
+
+    @Override
+    public void finish() {
+        Intent data = new Intent();
+        String feedback = "OK, Hello " + fullName + ". How are you?";
+        data.putExtra("feedback", feedback);
+
+        setResult(Activity.RESULT_OK, data);
+        super.finish();
+    }
+}
