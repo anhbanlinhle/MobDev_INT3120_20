abc16fb103e38f40e1a8abd1881c6eea0c9f9fc6
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-11 16:33:08 +0700

    add logic handler for sms

--- slide_12/app/src/main/java/com/example/slide_12/telephony/sms/SmsActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/telephony/sms/SmsActivity.java
@@ -1,8 +1,12 @@
 package com.example.slide_12.telephony.sms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,5 +19,26 @@ public class SmsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+
+
+        phone = findViewById(R.id.phoneNumber);
+        message = findViewById(R.id.message);
+        send = findViewById(R.id.sendMessage);
+
+        if (ActivityCompat.checkSelfPermission(SmsActivity.this, android.Manifest.permission.SEND_SMS) != android.content.pm.PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(SmsActivity.this, new String[]{android.Manifest.permission.SEND_SMS}, 1);
+        }
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phone.getText().toString();
+                String messageContent = message.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+                intent.putExtra("sms_body", messageContent);
+                startActivity(intent);
+            }
+        });
     }
 }
