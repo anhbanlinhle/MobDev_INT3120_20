04e00492fc026ddc63f50f64e4f87e12ecc792bf
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-11 15:27:01 +0700

    base layout for telephony call

--- slide_12/app/src/main/java/com/example/slide_12/telephony/CallActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/telephony/CallActivity.java
@@ -1,8 +1,10 @@
-package com.example.slide_12;
+package com.example.slide_12.telephony;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.slide_12.R;
 
 public class CallActivity extends AppCompatActivity {
 
