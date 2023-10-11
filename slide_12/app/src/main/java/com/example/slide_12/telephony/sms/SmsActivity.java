1f1504964a65a4c55702b50c5d2afb5cd13c2dbb
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-11 16:27:49 +0700

    base layout for sms

--- slide_12/app/src/main/java/com/example/slide_12/telephony/sms/SmsActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/telephony/sms/SmsActivity.java
@@ -3,11 +3,14 @@ package com.example.slide_12.telephony.sms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.slide_12.R;
 
 public class SmsActivity extends AppCompatActivity {
-
+    private EditText phone, message;
+    private Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
