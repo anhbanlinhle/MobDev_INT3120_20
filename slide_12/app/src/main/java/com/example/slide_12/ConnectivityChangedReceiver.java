68b6a52ce8cccf625b8e24fdcaf66dfb8e8a1f52
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-11 14:57:19 +0700

    add receiver and permissions

---
+++ slide_12/app/src/main/java/com/example/slide_12/ConnectivityChangedReceiver.java
@@ -0,0 +1,45 @@
+package com.example.slide_12;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import java.util.List;
+
+public class ConnectivityChangedReceiver extends BroadcastReceiver {
+    private WifiManager mWifiManager;
+    private List<String> wifiList;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
+            int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
+            if (wifiState == WifiManager.WIFI_STATE_ENABLED) {
+                Toast.makeText(context, "Wifi Enabled", Toast.LENGTH_LONG).show();
+            }
+            else if (wifiState == WifiManager.WIFI_STATE_DISABLED) {
+                    Toast.makeText(context, "Wifi Disabled", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        else if (intent.getAction() != null && intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(WifiActivity.wifiActivity, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                return;
+            }
+
+            mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            List<ScanResult> scanResults = mWifiManager.getScanResults();
+            for (ScanResult scanResult : scanResults) {
+                wifiList.add(scanResult.SSID);
+            }
+            WifiActivity.listView.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, wifiList));
+        }
+    }
+}
--- slide_12/app/src/main/java/com/example/slide_12/WifiActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/WifiActivity.java
@@ -2,9 +2,19 @@ package com.example.slide_12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class WifiActivity extends AppCompatActivity {
+    public static Activity wifiActivity;
+    private WifiManager mWifiManager;
+    private TextView networkInfoTextView, wifiInfoTextView, status;
+    public static ListView listView;
+    private ConnectivityManager mConnectivityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
