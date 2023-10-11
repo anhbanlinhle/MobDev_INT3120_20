2053763ad9231888632a873111370eaba7ce01c4
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-12 03:00:22 +0700

    add logic handler for map

--- slide_13/app/src/main/java/com/example/slide_13/MapActivity.java
+++ slide_13/app/src/main/java/com/example/slide_13/MapActivity.java
@@ -12,15 +12,40 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
-
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
 
+        if (getIntent().hasExtra("latitude") && getIntent().hasExtra("longitude")) {
+            LatLng currentLocation = new LatLng(getIntent().getDoubleExtra("latitude", 0), getIntent().getDoubleExtra("longitude", 0));
+            mMap.addMarker(new MarkerOptions()
+                    .position(currentLocation)
+                    .title("Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        }
+        else {
+            LatLng vancouver = new LatLng(49.246292, -123.116226);
+            mMap.addMarker(new MarkerOptions()
+                    .position(vancouver)
+                    .title("Marker in Vancouver"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(vancouver));
+        }
     }
 }
