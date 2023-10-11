357bf1f25fcd2d82b08f620a20ba6a5ea059299e
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-12 03:12:13 +0700

    add logic handler for webview

--- slide_14/app/src/main/java/com/example/slide_14/MainActivity.java
+++ slide_14/app/src/main/java/com/example/slide_14/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.slide_14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -11,5 +15,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://github.com/anhbanlinhle");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    public class MyWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        if ((keyCode == KeyEvent.KEYCODE_FORWARD) && webView.canGoForward()) {
+            webView.goForward();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
