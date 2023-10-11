959585a213c40055b5df84fe154c73c905ba2f9e
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-11 15:16:16 +0700

    handle logic for wifi activity

--- slide_12/app/src/main/java/com/example/slide_12/WifiActivity.java
+++ slide_12/app/src/main/java/com/example/slide_12/WifiActivity.java
@@ -1,13 +1,26 @@
 package com.example.slide_12;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WifiActivity extends AppCompatActivity {
     public static Activity wifiActivity;
@@ -15,10 +28,59 @@ public class WifiActivity extends AppCompatActivity {
     private TextView networkInfoTextView, wifiInfoTextView, status;
     public static ListView listView;
     private ConnectivityManager mConnectivityManager;
+    ConnectivityChangedReceiver receiver = new ConnectivityChangedReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
+
+        mConnectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = mConnectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            networkInfoTextView = findViewById(R.id.wifiStatus);
+            networkInfoTextView.setText("Network Info: " + networkInfo.toString());
+        }
+        else {
+            Toast.makeText(this, "Not connected", Toast.LENGTH_LONG).show();
+        }
+
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiInfoTextView = findViewById(R.id.wifiInfo);
+        wifiInfoTextView.setText("Wifi Info: " + mWifiManager.getConnectionInfo().toString());
+
+        registerReceiver(receiver, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
+        mWifiManager.startScan();
+        //List of Wifi Networks
+        Button scanButton = findViewById(R.id.scan_wifi);
+        listView = findViewById(R.id.wifiList);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(WifiActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(WifiActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                } else {
+                    mWifiManager.startScan();
+                    List<ScanResult> scanResults = mWifiManager.getScanResults();
+                    List<String> wifiList = new ArrayList<>();
+                    for (ScanResult scanResult : scanResults) {
+                        wifiList.add(scanResult.SSID);
+                    }
+                    listView.setAdapter(new ArrayAdapter<>(WifiActivity.this, android.R.layout.simple_list_item_1, wifiList));
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        wifiActivity = this;
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 }
