a658a022d1323118a002ae406254acfd78c1fdfa
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-12 03:05:28 +0700

    base layout for webview

--- slide_14/app/src/main/java/com/example/slide_14/MainActivity.java
+++ slide_14/app/src/main/java/com/example/slide_14/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
