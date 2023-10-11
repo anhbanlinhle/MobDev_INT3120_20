92fc1d3fd66e68676cd2e58de2f2833e7962175c
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-12 01:15:36 +0700

    base layout for main activity

--- slide_13/app/src/main/java/com/example/slide_13/MainActivity.java
+++ slide_13/app/src/main/java/com/example/slide_13/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.slide_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toMedia(View view) {
+
+    }
+
+    public void toLocation(View view) {
+
+    }
+
+    public void toMap(View view) {
+
+    }
+
 }
