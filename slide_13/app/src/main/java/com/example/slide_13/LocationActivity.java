46192a92fde16b08e155bb2ed82fea1c4a2b8a32
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-12 02:37:38 +0700

    base layout for location

--- slide_13/app/src/main/java/com/example/slide_13/LocationActivity.java
+++ slide_13/app/src/main/java/com/example/slide_13/LocationActivity.java
@@ -1,14 +1,22 @@
 package com.example.slide_13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
     }
 }
