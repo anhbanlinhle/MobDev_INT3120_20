d8cfd774561bac1bd70c78fdb80470d3b18aea1c
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-12 02:58:15 +0700

    add base layout for map

--- slide_13/app/src/main/java/com/example/slide_13/MapActivity.java
+++ slide_13/app/src/main/java/com/example/slide_13/MapActivity.java
@@ -1,14 +1,26 @@
 package com.example.slide_13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
